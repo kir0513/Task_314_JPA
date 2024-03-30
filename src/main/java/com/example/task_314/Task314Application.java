02e0204fbb5d5ba381e5045502152b350bcb6b04
kir0513 <kir0513@yandex.ru>
2024-03-30 19:11:25 +0300

    Initial commit. Изменить страницы вашего приложения в соответствии со скриншотами, используя Bootstrap.

---
+++ src/main/java/com/example/task_314/Task314Application.java
@@ -0,0 +1,13 @@
+package com.example.task_314;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task314Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task314Application.class, args);
+    }
+
+}
